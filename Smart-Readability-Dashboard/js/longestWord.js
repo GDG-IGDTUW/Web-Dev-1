792d46b002f149477f972d11953dce6b3d4bf085
urvidixit <urvidixit1@gmail.com>
2026-01-21 02:08:22 +0530

    Create longestWord.js

---
+++ Smart-Readability-Dashboard/js/longestWord.js
@@ -0,0 +1,11 @@
+function findLongestWord(str) {
+    let words = str.split(" ");
+    let longestWord = "";
+
+    for (let word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+    return longestWord;
+}
